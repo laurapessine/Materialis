030665c51ed51a59a6a75f2a975bdf0e948c05ee
MariaEduarda-Moura <mariaeduardam.crusco@hotmail.com>
2025-06-16 16:18:12 -0300

    Adicionando tabela estudante

--- db/MySQL/create.sql
+++ db/MySQL/create.sql
@@ -1,4 +1,8 @@
+DROP DATABASE IF EXISTS Materialis;
+
 CREATE DATABASE Materialis;
+
+USE Materialis;
 
 DROP TABLE IF EXISTS Material;
 CREATE TABLE Material (
@@ -12,3 +16,17 @@ CREATE TABLE Material (
     criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     atualizado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+DROP TABLE IF EXISTS Estudante;
+CREATE TABLE Estudante(
+    id INT NOT NULL auto_increment PRIMARY KEY, 
+    nome VARCHAR(256) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    senha VARCHAR(15) NOT NULL,
+    CPF INT(11) NOT NULL,
+    RA INT(6) NOT NULL,
+    telefone INT(11) NOT NULL,
+    sexo ENUM('Feminino', 'Masculino', 'Outro') NOT NULL,
+    nascimento INT(8) NOT NULL);
+
+INSERT INTO Estudante(nome, email, senha, CPF, RA, telefone, sexo, nascimento) VALUES ('admin','admin@gmail.com','admin', 12345678910, 82000, 16912345678, 'Feminino', 11032000);
