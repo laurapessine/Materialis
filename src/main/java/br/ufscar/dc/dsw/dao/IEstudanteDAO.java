1917fbdc3747a9468b14816f76b89f4411c1e8f0
MariaEduarda-Moura <mariaeduardam.crusco@hotmail.com>
2025-06-16 17:12:25 -0300

    DAO IEstudanteDAO

---
+++ src/main/java/br/ufscar/dc/dsw/dao/IEstudanteDAO.java
@@ -0,0 +1,14 @@
+package br.ufscar.dc.dsw.dao;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import br.ufscar.dc.dsw.domain.Estudante;
+
+@SuppressWarnings("unchecked")
+public interface IEstudanteDAO extends CrudRepository<Estudante, Long> {
+    Estudante findById(long id);
+    Estudante findByCPF (String CPF);
+	List<Estudante> findAll();
+	Estudante save(Estudante estudante);
+	void deleteById(Long id);
+}
